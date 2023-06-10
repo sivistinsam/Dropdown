1c120f30a581713a5fe34438db60e2824c73cf3e
Sandeep Kumar Gupta <57314972+sivistinsam@users.noreply.github.com>
2023-06-10 19:42:36 +0530

    Update App.js
    checking

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       {/* Created a separate Dropdown component and accessing it here*/}
+      <h1>Hello</h1>
       <Dropdwon />
     </div>
   );
